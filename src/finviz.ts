9971b5cee060cd77d01333fe5ebac1338143b875
Sean Soper <sean.soper@gmail.com>
2022-09-11 19:41:58 -0400

    remove navbar

--- src/finviz.ts
+++ src/finviz.ts
@@ -11,18 +11,22 @@ export const showSpyMap = async (page: puppeteer.Page, delaySecs?: number): Prom
     await delay(delaySecs || DefaultDelaySecs);
 }
 
-export const showFutures = async (page: puppeteer.Page, delaySecs?: number): Promise<void> => {
-    await page.goto('https://finviz.com/futures.ashx');
+const showPage = async (page: puppeteer.Page, url: string, delaySecs?: number): Promise<void> => {
+    await page.goto(url);
     await page.$eval('table.header', (e) => {
         e.remove();
     })
+    await page.$eval('table.navbar', (e) => {
+        e.remove();
+    })
+
     await delay(delaySecs || DefaultDelaySecs);
 }
 
+export const showFutures = async (page: puppeteer.Page, delaySecs?: number): Promise<void> => {
+    await showPage(page, 'https://finviz.com/futures.ashx', delaySecs);
+}
+
 export const showCrypto = async (page: puppeteer.Page, delaySecs?: number): Promise<void> => {
-    await page.goto('https://finviz.com/crypto.ashx');
-    await page.$eval('table.header', (e) => {
-        e.remove();
-    })
-    await delay(delaySecs || DefaultDelaySecs);
+    await showPage(page, 'https://finviz.com/crypto.ashx', delaySecs);
 }
