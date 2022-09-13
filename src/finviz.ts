b4691e9c106d8a4eadbe7922c0d42394748c818a
Sean Soper <sean.soper@gmail.com>
2022-09-13 09:31:10 -0400

    attempt to apply transform to maximize scale of crypto page

--- src/finviz.ts
+++ src/finviz.ts
@@ -1,17 +1,17 @@
 import * as puppeteer from "puppeteer";
 import { delay } from "./delay";
 
-const DefaultDelaySecs = 3;
+const DefaultDelaySecs = process.env.NODE_ENV === 'production' ? 300 : 10;
 
-export const showSpyMap = async (page: puppeteer.Page, delaySecs?: number): Promise<void> => {
+export const showSpyMap = async (page: puppeteer.Page, delaySecs: number = DefaultDelaySecs): Promise<void> => {
     await page.goto('https://finviz.com/map.ashx');
     await page.$eval('body', (e) => {
         e.classList.add('is-map-expanded');
     })
-    await delay(delaySecs || DefaultDelaySecs);
+    await delay(delaySecs);
 }
 
-const showPage = async (page: puppeteer.Page, url: string, delaySecs?: number): Promise<void> => {
+const showPage = async (page: puppeteer.Page, url: string): Promise<void> => {
     await page.goto(url);
     await page.$eval('table.header', (e) => {
         e.remove();
@@ -19,14 +19,18 @@ const showPage = async (page: puppeteer.Page, url: string, delaySecs?: number): 
     await page.$eval('table.navbar', (e) => {
         e.remove();
     })
-
-    await delay(delaySecs || DefaultDelaySecs);
 }
 
-export const showFutures = async (page: puppeteer.Page, delaySecs?: number): Promise<void> => {
-    await showPage(page, 'https://finviz.com/futures.ashx', delaySecs);
+export const showFutures = async (page: puppeteer.Page, delaySecs: number = DefaultDelaySecs): Promise<void> => {
+    await showPage(page, 'https://finviz.com/futures.ashx');
+    await delay(delaySecs);
 }
 
-export const showCrypto = async (page: puppeteer.Page, delaySecs?: number): Promise<void> => {
-    await showPage(page, 'https://finviz.com/crypto.ashx', delaySecs);
+export const showCrypto = async (page: puppeteer.Page, delaySecs: number = DefaultDelaySecs): Promise<void> => {
+    await showPage(page, 'https://finviz.com/crypto.ashx');
+    await page.$eval('div.content > div > div > div > div:nth-child(2)', (e) => {
+        const style = e.getAttribute('style') as string;
+        e.setAttribute('style', `${style}; transform: scale(1.5); margin-top: 100px;`)
+    })
+    await delay(delaySecs);
 }
