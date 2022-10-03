608ead09a70ec0ffe249368dab51ba45ca48043a
Lint Action <lint-action@samuelmeuli.com>
2022-10-03 19:23:46 +0000

    Fix code style issues with ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,17 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: ['standard-with-typescript', 'prettier'],
-  plugins: ['prettier'],
-  overrides: [
-  ],
+  extends: ["standard-with-typescript", "prettier"],
+  plugins: ["prettier"],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: `./tsconfig.json`
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: `./tsconfig.json`,
   },
   rules: {
-    'prettier/prettier': ['error']
-  }
-}
+    "prettier/prettier": ["error"],
+  },
+};
