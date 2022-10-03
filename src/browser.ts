694a5330a925c72df88addb67cbf293ff8bfd115
Sean Soper <sean.soper@gmail.com>
2022-10-03 15:55:11 -0400

    headless

--- src/browser.ts
+++ src/browser.ts
@@ -4,16 +4,16 @@ import { executablePath } from "./chrome";
 const ChromiumPort = process.env.CHROMIUM_PORT ?? "9123";
 
 export const getBrowser = async (): Promise<puppeteer.Browser> => {
-  if (process.env.NODE_ENV === "production") {
-    const browserURL = `http://127.0.0.1:${ChromiumPort}`;
-    return await puppeteer.connect({ browserURL });
-  } else {
-    const options = {
-      headless: false,
-      defaultViewport: null,
-      executablePath: executablePath(),
-    };
+  const options: any = {
+    headless: false,
+    defaultViewport: null,
+  };
 
+  if (process.env.NODE_ENV === "production") {
+    options.browserURL = `http://127.0.0.1:${ChromiumPort}`;
+    return await puppeteer.connect(options);
+  } else {
+    options.executablePath = executablePath();
     return await puppeteer.launch(options);
   }
 };
