e7b2c8bf6a50c32ebf57fd10d28c5407a6a674ab
Sean Soper <sean.soper@gmail.com>
2022-10-04 09:32:36 -0400

    ensure lint really does lint everything using node file glob, ignore iife rule in index

--- index.ts
+++ index.ts
@@ -1,34 +1,33 @@
-import { logger } from './src/logger';
-import {
-    showSpyMap,
-    showFutures,
-    showCrypto
-} from './src/finviz';
-import { getBrowser } from './src/browser';
-import { isHeatMapAvailable } from './src/market';
-import { Browser } from 'puppeteer-core';
+/* eslint @typescript-eslint/no-floating-promises: "off" */
+import { logger } from "./src/logger";
+import { showSpyMap, showFutures, showCrypto } from "./src/finviz";
+import { getBrowser } from "./src/browser";
+import { isHeatMapAvailable } from "./src/market";
+import { Browser } from "puppeteer-core";
 
 (async () => {
-    process.on('SIGINT', async () => {
-        logger.info('Aborted')
-        await browser.close();
-        process.exit();
-    })
+  process.on("SIGINT", (error: Error) => {
+    (async () => {
+      logger.error(error);
+      await browser.close();
+      process.exit();
+    })();
+  });
 
-    let browser: Browser
+  let browser: Browser;
 
-    try {
-        browser = await getBrowser();
-        const page = await browser.newPage();
+  try {
+    browser = await getBrowser();
+    const page = await browser.newPage();
 
-        while (true) {
-            if (isHeatMapAvailable()) {
-                await showSpyMap(page);
-            }
-            await showFutures(page);
-            await showCrypto(page);
-        }
-    } catch (error: any) {
-        logger.error(error)
+    while (true) {
+      if (isHeatMapAvailable()) {
+        await showSpyMap(page);
+      }
+      await showFutures(page);
+      await showCrypto(page);
     }
+  } catch (error: any) {
+    logger.error(error);
+  }
 })();
